4b96d89c5ea37bf5a570f4400c56645624f01ca1
Brigitte G <urbrigette@gmail.com>
2020-10-09 23:22:32 -0400

    comment changes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,15 +1,15 @@
 
-// Requiering path
+    // Requiering path
 const path = require("path");
 
 module.exports = function (app) {
 
-    //   Connecting to 'index.html'
+    // Get request to 'index.html'
     app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
     });
 
-    // Connectiong to 'notes.html'
+    // Get request  to 'notes.html'
     app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require("express");
 // Express
 const app = express();
+
 // Port local host 3000
 const PORT = process.env.PORT || 3000;
 
@@ -12,11 +13,12 @@ app.use(express.urlencoded({ extended: true }));
 
 
 app.use(express.static("public"));
-//Routes 
+
+// Routes 
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes.js")(app);
-// Listening for client request
 
+// Listening for client request
 app.listen(PORT, ()=> console.log('Listening on: http://localhost:'+ PORT))
 
 
